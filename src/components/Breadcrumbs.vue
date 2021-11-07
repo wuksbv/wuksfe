ac87344a6ace5c3361e71585a7500067c6b50bd7
WUKS TPM <gkhop2014@gmail.com>
2021-11-07 21:04:31 +0100

    First commit

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    parentTitle: String,
+    subParentTitle: String
+})
+</script>
+
+<template>
+    <div class="breadcrumb flex items-end border-b border-white-300 pb-4 mb-6">
+        <p class="text-xl mr-1 font-semibold mr-2 dark:text-white"> {{ props.parentTitle }} </p>
+        <ul>
+            <li class="border-r border-white-400 inline pr-2 dark:text-white-400"><a class="hover:text-white-800" href=""> {{ props.parentTitle }}</a></li>
+            <li class="inline pl-2 dark:text-white-400"> {{ props.subParentTitle }}</li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+    .breadcrumb{
+        color:white
+    }
+</style>
+
---
+++ src/layout/Footer.vue
@@ -0,0 +1,36 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <footer class="mt-10">
+        <div class="container-fluid mx-auto">
+            <BaseCard class="">
+                <div class="flex justify-between items-center">
+                    <a
+                        target="_blank"
+                        href="https://aatrox-vue.netlify.app/"
+                        style="position: fixed;
+                         bottom: 14px; right: 80px"
+                        class="
+                            py-3
+                            px-5
+                            inline-block
+                            bg-green-500
+                            text-white
+                            btn
+                            rounded
+                            font-normal
+                            leading-4
+                            ripple
+                        "
+                        >Get Aatrox Pro</a
+                    >
+                    <div>
+                        <p>All rights reserved © UI Lib 2021</p>
+                    </div>
+                </div>
+            </BaseCard>
+        </div>
+    </footer>
+</template>
